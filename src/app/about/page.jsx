faab570552e233af0688b0fffc0178d749483a1f
Alberto Toledo <a.toledomayorga@gmail.com>
2023-03-17 16:05:24 +0100

    Added Navbar component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <>
-      <div className="max-w-6xl mx-auto space-y-4 space-x-2">
+      <div className="max-w-6xl mx-auto space-y-4 p-4">
         <h1 className="text-2xl font-medium text-amber-600">About</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
@@ -11,14 +11,14 @@ const About = () => {
           vel sed ligula. Duis nec sodales dolor. Integer euismod eget metus a
           lobortis. Integer venenatis lacus nunc, id accumsan urna luctus
           suscipit. Pellentesque ante risus, convallis a tortor sit amet,
-          euismod vulputate quam. 
+          euismod vulputate quam.
         </p>
         <p>
           Praesent et mattis nisi, et dignissim sapien. Proin placerat, odio non
           placerat elementum, justo arcu commodo odio, et maximus tellus augue
           ut enim. Etiam aliquet, turpis in ultricies condimentum, ligula purus
           commodo arcu, nec facilisis eros dolor tempor lorem. Nam tempor odio
-          sapien, sit amet dictum tellus gravida sed. 
+          sapien, sit amet dictum tellus gravida sed.
         </p>
       </div>
     </>
