1f6d8b0e2bb8b29ad76c6c1bb9d8d1418f40f994
Alberto Toledo <a.toledomayorga@gmail.com>
2023-03-17 15:44:04 +0100

    Added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+const About = () => {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto space-y-4 space-x-2">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          tristique mauris eget sem commodo consequat. Duis tincidunt, tortor id
+          volutpat sollicitudin, metus orci ultricies nisl, imperdiet pretium
+          odio elit vel arcu. Aliquam ut ligula sit amet turpis pretium lobortis
+          vel sed ligula. Duis nec sodales dolor. Integer euismod eget metus a
+          lobortis. Integer venenatis lacus nunc, id accumsan urna luctus
+          suscipit. Pellentesque ante risus, convallis a tortor sit amet,
+          euismod vulputate quam. 
+        </p>
+        <p>
+          Praesent et mattis nisi, et dignissim sapien. Proin placerat, odio non
+          placerat elementum, justo arcu commodo odio, et maximus tellus augue
+          ut enim. Etiam aliquet, turpis in ultricies condimentum, ligula purus
+          commodo arcu, nec facilisis eros dolor tempor lorem. Nam tempor odio
+          sapien, sit amet dictum tellus gravida sed. 
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
